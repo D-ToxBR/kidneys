be2b2a5ee29ab9cffc39cc0e571ad2d5c8f77af2
CoinBR <coinbr@gmail.com>
2024-05-14 18:35:10 -0300

    first working version

---
+++ cfg/rankTitles.ts
@@ -0,0 +1,11 @@
+export default {
+    7: "All Stars",
+    6: "Platina",
+    5: "Ouro",
+    4: "Prata",
+    3: "Bronze",
+    2: "Cobre",
+    1: "Alumínio (Iniciante no CS)",
+    0: "Ferro (Iniciante em Jogos de Tiro)"
+};
+
