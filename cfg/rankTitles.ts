3ab1c84ac8e4487decceec400c72522eef9a40e3
CoinBR <coinbr@gmail.com>
2025-07-14 15:47:44 -0300

    update rankTitles to better explain players lvls

--- cfg/rankTitles.ts
+++ cfg/rankTitles.ts
@@ -1,14 +1,14 @@
 import type {RankTitles} from "../src/modules/rankProcessing/rankProcessing";
 
 const rankTitles: RankTitles = {
-    7: "All Stars",
-    6: "Platina",
-    5: "Ouro",
-    4: "Prata",
-    3: "Bronze",
-    2: "Cobre",
-    1: "Alumínio",
-    0: "Ferro"
+    7: "Estrela",
+    6: "Excelente",
+    5: "Avançado",
+    4: "Bom Player",
+    3: "Intermediário",
+    2: "Básico",
+    1: "Adaptando-se ao CS",
+    0: "Iniciante em Jogos"
 }
 
 export default  rankTitles
